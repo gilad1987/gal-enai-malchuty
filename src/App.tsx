58379ad887334d842dbeb70ecb23caf20cf9980a
Gilad takoni <gilad1987@gmail.com>
2020-02-18 22:26:48 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App: React.FC = () => {
     <ConfigProvider>
       <ThemeProvider theme={theme}>
         <Provider {...stores}>
-          <Router>
+          <Router  basename="/gal-enai-malchuty" >
             <Header />
             <Switch>
               <Route path="/" exact>
