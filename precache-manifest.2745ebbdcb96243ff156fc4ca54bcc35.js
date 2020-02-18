f43923941bfc40ce30e27223dc5a4f8b31ee911c
Gilad takoni <gilad1987@gmail.com>
2020-02-18 11:53:58 +0200

    Updates

---
+++ precache-manifest.2745ebbdcb96243ff156fc4ca54bcc35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77b568c9246579721dc461f0dd5dc26d",
+    "url": "/gilad1987/gal-enai-malchuty/index.html"
+  },
+  {
+    "revision": "61efbf2fc44c72939d88",
+    "url": "/gilad1987/gal-enai-malchuty/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "73d74c60b7caff173fcf",
+    "url": "/gilad1987/gal-enai-malchuty/static/css/main.97ba0edb.chunk.css"
+  },
+  {
+    "revision": "61efbf2fc44c72939d88",
+    "url": "/gilad1987/gal-enai-malchuty/static/js/2.9a7714f2.chunk.js"
+  },
+  {
+    "revision": "335932bcfe373407d7c8acc8ad15eddd",
+    "url": "/gilad1987/gal-enai-malchuty/static/js/2.9a7714f2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "73d74c60b7caff173fcf",
+    "url": "/gilad1987/gal-enai-malchuty/static/js/main.bcc7df0a.chunk.js"
+  },
+  {
+    "revision": "c49fbc38f7662adf92a8",
+    "url": "/gilad1987/gal-enai-malchuty/static/js/runtime-main.a7695fbc.js"
+  }
+]);
